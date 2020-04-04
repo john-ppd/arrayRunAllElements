dbcd732820234486b713e66f328820b13b7a6ea2
john-ppd <63131902+john-ppd@users.noreply.github.com>
2020-04-03 20:37:11 -0400

    Add project files.

---
+++ Project8/Source.cpp
@@ -0,0 +1,28 @@
+/*
+ * rangeBasedFor.cpp
+ *
+ *  Created on: Mar 10, 2020
+ *      Author: Elgazzar
+ */
+
+
+#include <iostream>
+using namespace std;
+double sum = 0.0;
+int main() {
+
+   // double time[300] = { 0.0,0.1,0.2,0.3,0.4 };
+
+    //for (double t : time)//displays them all!
+       // cout << t << endl;
+
+    for (int i = 1; i <= 50; i++)
+    {
+        sum = sum + (1.0 / (i * i));
+        cout <<"i :" << i << " "<< sum <<endl;
+    }
+    cout << sum;
+
+    return 0;
+}
+
